2868a427120f02a82766cacc0393f34761556547
Alok Hom <alok.hom@gmail.com>
2024-11-10 23:09:24 +0100

    Update test.py

--- dags/github_archive/test.py
+++ dags/github_archive/test.py
@@ -4,6 +4,7 @@ from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.operators.bash import BashOperator
 from random import randint
 from datetime import datetime
+import pendulum
 
 
 def _import_process(_file):
