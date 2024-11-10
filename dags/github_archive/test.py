7760f1a029ca027f84a5880ed376b0da179c68c0
Alok Hom <alok.hom@gmail.com>
2024-11-11 00:52:21 +0100

    Update test.py

--- dags/github_archive/test.py
+++ dags/github_archive/test.py
@@ -76,14 +76,17 @@ with DAG(dag_id="yajl_dag", start_date=pendulum.datetime(2024,11,0o7,tz="CET"), 
     
        # january
        getfiles_jan = BashOperator(task_id="getfiles_jan",bash_command="wget https://data.gharchive.org/2024-01-01-{0..23}.json.gz | gzip -d > 2024-1.json")
+       importall_jan = PythonOperator(task_id="jan_process",python_callable=check_process,op_kwargs={"file_name": 2024-1.json"})
 
        # feb
        getfiles_feb = BashOperator(task_id="getfiles_feb",bash_command="wget https://data.gharchive.org/2024-02-01-{0..23}.json.gz | gzip -d > 2024-2.json")
+       importall_feb = PythonOperator(task_id="feb_process",python_callable=check_process,op_kwargs={"file_name": 2024-2.json"})
 
        # mar
        getfiles_mar = BashOperator(task_id="getfiles_mar",bash_command="wget https://data.gharchive.org/2024-03-01-{0..23}.json.gz | gzip -d > 2024-3.json")
+       importall_mar = PythonOperator(task_id="mar_process",python_callable=check_process,op_kwargs={"file_name": 2024-3.json"})
        
-       for i in range(1, 4):
-           importall = PythonOperator(task_id=f"{i}_process",python_callable=check_process,op_kwargs={"file_name": f"2024-{i}.json"})
+       #for i in range(1, 4):
+       #    importall = PythonOperator(task_id=f"{i}_process",python_callable=check_process,op_kwargs={"file_name": f"2024-{i}.json"})
 
-       [pypacks,getfiles_jan,getfiles_feb,getfiles_mar] >> importall
+       pypacks >> [getfiles_jan,importall_jan] >> [getfiles_feb,importall_feb] >> [getfiles_mar,importall_mar] 
