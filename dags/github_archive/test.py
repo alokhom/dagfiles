b5bf2b9384c1204ab9b91e473d175c9f0d8df5d8
Alok Hom <alok.hom@gmail.com>
2024-11-10 18:47:36 +0000

    add new sample

---
+++ dags/github_archive/test.py
@@ -0,0 +1,82 @@
+from airflow import DAG
+from airflow.decorators import task
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.operators.bash import BashOperator
+from random import randint
+from datetime import datetime
+
+
+def _import_process(_file):
+    import os
+    import sys
+    #import io
+    #import requests
+    #import gzip
+    #import json
+
+    BASEPATH = os.path.dirname(os.path.realpath(__file__))
+    sys.path = [BASEPATH, '%s/..' %BASEPATH] + sys.path
+    from yajl import *
+
+    # Sample callbacks, which output some debug info
+    # these are examples to show off the yajl parser
+    class ContentHandler(YajlContentHandler):
+        def __init__(self):
+            self.out = sys.stdout
+        def yajl_null(self, ctx):
+            self.out.write("null\n" )
+        def yajl_boolean(self, ctx, boolVal):
+            self.out.write("bool: %s\n" %('true' if boolVal else 'false'))
+        def yajl_integer(self, ctx, integerVal):
+            self.out.write("integer: %s\n" %integerVal)
+        def yajl_double(self, ctx, doubleVal):
+            self.out.write("double: %s\n" %doubleVal)
+        def yajl_number(self, ctx, stringNum):
+            ''' Since this is defined both integer and double callbacks are useless '''
+            num = float(stringNum) if '.' in stringNum else int(stringNum)
+            self.out.write("number: %s\n" %num)
+        def yajl_string(self, ctx, stringVal):
+            self.out.write("string: '%s'\n" %stringVal)
+        def yajl_start_map(self, ctx):
+            self.out.write("map open '{'\n")
+        def yajl_map_key(self, ctx, stringVal):
+            self.out.write("key: '%s'\n" %stringVal)
+        def yajl_end_map(self, ctx):
+            self.out.write("map close '}'\n")
+        def yajl_start_array(self, ctx):
+            self.out.write("array open '['\n")
+        def yajl_end_array(self, ctx):
+            self.out.write("array close ']'\n")
+
+
+    def main(args):
+        parser = YajlParser(ContentHandler())
+        parser.allow_multiple_values = True
+        if args:
+            for fn in args:
+                f = open(fn)
+                parser.parse(f=f)
+                f.close()
+        else:
+            parser.parse()
+        return 0
+
+    if __name__ == "__main__":
+        raise SystemExit(main(sys.argv[1:]))
+
+
+with DAG(dag_id="my_dag", start_date=pendulum.datetime(2024,11,0o9,tz="CET"), schedule_interval='@hourly', catchup=False) as dag:
+
+       # january
+       getfiles_jan = BashOperator(task_id="getfiles_jan",bash_command="wget https://data.gharchive.org/2024-01-{01..31}-{0..23}.json.gz | gzip -d > 2024-1.json")
+
+       # feb
+       getfiles_feb = BashOperator(task_id="getfiles_feb",bash_command="wget https://data.gharchive.org/2024-02-{01..29}-{0..23}.json.gz | gzip -d > 2024-2.json")
+
+       # mar
+       getfiles_mar = BashOperator(task_id="getfiles_mar",bash_command="wget https://data.gharchive.org/2024-03-{01..31}-{0..23}.json.gz | gzip -d > 2024-3.json")
+       
+       for i in range(1, 4):
+           importall = PythonOperator(task_id="import_process",python_callable=_import_process,op_kwargs={"_file": 2024-i.json})
+
+       [getfiles_jan,getfiles_feb,getfiles_mar] >> importall
--- dags/sample/my_dag.py
+++ dags/sample/my_dag.py
@@ -22,20 +22,11 @@ def _training_model():
 
 with DAG(dag_id="my_dag", start_date=pendulum.datetime(2024,11,0o7,tz="CET"), schedule_interval='@hourly', catchup=False) as dag:
 
-       training_model_A = PythonOperator(
-           task_id="training_model_A", 
-           python_callable=_training_model
-        )
+       training_model_A = PythonOperator(task_id="training_model_A",python_callable=_training_model)
 
-       training_model_B = PythonOperator(
-           task_id="training_model_B", 
-           python_callable=_training_model
-        )
+       training_model_B = PythonOperator(task_id="training_model_B",python_callable=_training_model)
 
-       training_model_C = PythonOperator(
-           task_id="training_model_C", 
-           python_callable=_training_model
-        )
+       training_model_C = PythonOperator(task_id="training_model_C",python_callable=_training_model)
 
        choose_best_model = BranchPythonOperator(task_id="choose_best_model", python_callable=_choose_best_model)
 
