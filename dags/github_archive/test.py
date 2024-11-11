e845197aeee61452818687836145fe6fe9afed83
Alok Hom <alok.hom@gmail.com>
2024-11-11 01:24:49 +0100

    Update test.py

--- dags/github_archive/test.py
+++ dags/github_archive/test.py
@@ -66,7 +66,7 @@ def check_process(_file):
         raise SystemExit(main(sys.argv[1:]))
 
 
-with DAG(dag_id="yajl_dag", start_date=pendulum.datetime(2024,11,0o7,tz="CET"), schedule_interval='@hourly', catchup=False) as dag:
+with DAG(dag_id="yajl_dag_new", start_date=pendulum.datetime(2024,11,0o7,tz="CET"), schedule_interval='@hourly', catchup=False) as dag:
 
        # install python packages and build them
        pypacks = BashOperator(
