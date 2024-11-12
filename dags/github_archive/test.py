4eaf9aeb05798c92edc46e261b0f3d85e45bd81d
Alok Hom <alok.hom@gmail.com>
2024-11-12 22:16:45 +0100

    Update test.py

--- dags/github_archive/test.py
+++ dags/github_archive/test.py
@@ -9,16 +9,16 @@ import pendulum
 
 with DAG(dag_id="yajl_dag_new", start_date=pendulum.datetime(2024,11,0o7,tz="CET"), schedule_interval='@hourly', catchup=False) as dag:
  
-       # january
-       getfiles_jan = BashOperator(task_id="getfiles_jan",bash_command="curl -L https://data.gharchive.org/2024-01-01-23.json.gz | gzip -d > 2024-1.json")
+       # january | gzip -d > 2024-1.json
+       getfiles_jan = BashOperator(task_id="getfiles_jan",bash_command="curl -L https://data.gharchive.org/2024-01-01-23.json.gz")
        # importall_jan = PythonOperator(task_id="jan_process",python_callable=check_process,op_kwargs={"file_name": "2024-1.json"})
 
-       # feb
-       getfiles_feb = BashOperator(task_id="getfiles_feb",bash_command="curl -L https://data.gharchive.org/2024-02-01-23.json.gz | gzip -d > 2024-2.json")
+       # feb | gzip -d > 2024-2.json
+       getfiles_feb = BashOperator(task_id="getfiles_feb",bash_command="curl -L https://data.gharchive.org/2024-02-01-23.json.gz")
        # importall_feb = PythonOperator(task_id="feb_process",python_callable=check_process,op_kwargs={"file_name": "2024-2.json"})
 
-       # mar
-       getfiles_mar = BashOperator(task_id="getfiles_mar",bash_command="curl -L https://data.gharchive.org/2024-03-01-23.json.gz | gzip -d > 2024-3.json")
+       # mar | gzip -d > 2024-3.json
+       getfiles_mar = BashOperator(task_id="getfiles_mar",bash_command="curl -L https://data.gharchive.org/2024-03-01-23.json.gz")
        # importall_mar = PythonOperator(task_id="mar_process",python_callable=check_process,op_kwargs={"file_name": "2024-3.json"})
        
        #for i in range(1, 4):
