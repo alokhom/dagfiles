ba823e5015aa16a58201dd42215fb15f93a83244
Alok Hom <alok.hom@gmail.com>
2024-11-14 00:06:55 +0100

    Update test.py

--- dags/github_archive/test.py
+++ dags/github_archive/test.py
@@ -3,7 +3,7 @@ from airflow.decorators import task
 from airflow.exceptions import AirflowException
 from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.operators.bash import BashOperator
-from airflow.operators.postgres_operator import PostgresOperator 
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from airflow.utils.dates import days_ago
 from random import randint
 from datetime import datetime
@@ -64,9 +64,46 @@ with DAG(dag_id="yajl_dag_new", start_date=pendulum.datetime(2024,11,0o7,tz="CET
        # mar
        getfiles_mar = PythonOperator(task_id="getfiles_mar", python_callable=my_func, op_args=['https://data.gharchive.org/2024-03-01-23.json.gz'])
 
-       # db
-       execdb = PythonOperator(task_id="execdb", python_callable=execdb, op_args=['\c archive'])
-
-    
-       getfiles_jan >> getfiles_jancheck >> getfiles_feb >> getfiles_mar >> execdb
+       getfiles_jan >> getfiles_jancheck >> getfiles_feb >> getfiles_mar
        #importall_feb >> importall_jan >> importall_mar 
+
+# db
+#execdb = PythonOperator(task_id="execdb", python_callable=execdb, op_args=['\c archive'])
+with DAG(dag_id = "postgresop_demo", description='use case of operator in airflow', start_date = airflow.utils.dates.days_ago(1), schedule="@once",catchup=False) as dag:
+       create_pet_table = SQLExecuteQueryOperator(
+           task_id="create_pet_table",
+           conn_id="postgres_conn",
+           sql="""
+               CREATE TABLE IF NOT EXISTS pet (
+               pet_id SERIAL PRIMARY KEY,
+               name VARCHAR NOT NULL,
+               pet_type VARCHAR NOT NULL,
+               birth_date DATE NOT NULL,
+               OWNER VARCHAR NOT NULL);
+             """,
+       )
+       populate_pet_table = SQLExecuteQueryOperator(
+           task_id="populate_pet_table",
+           conn_id="postgres_conn",
+           sql="""
+                INSERT INTO pet (name, pet_type, birth_date, OWNER)
+                VALUES ( 'Max', 'Dog', '2018-07-05', 'Jane');
+                INSERT INTO pet (name, pet_type, birth_date, OWNER)
+                VALUES ( 'Susie', 'Cat', '2019-05-01', 'Phil');
+                INSERT INTO pet (name, pet_type, birth_date, OWNER)
+                VALUES ( 'Lester', 'Hamster', '2020-06-23', 'Lily');
+                INSERT INTO pet (name, pet_type, birth_date, OWNER)
+                VALUES ( 'Quincy', 'Parrot', '2013-08-11', 'Anne');
+               """,
+       )
+       get_all_pets = SQLExecuteQueryOperator(task_id="get_all_pets", sql="SELECT * FROM pet;")
+       get_birth_date = SQLExecuteQueryOperator(
+           task_id="get_birth_date",
+           conn_id="postgres_conn",
+           sql="SELECT * FROM pet WHERE birth_date BETWEEN SYMMETRIC %(begin_date)s AND %(end_date)s",
+           parameters={"begin_date": "2020-01-01", "end_date": "2020-12-31"},
+           hook_params={"options": "-c statement_timeout=3000ms"},
+        )
+       
+       create_pet_table >> populate_pet_table >> get_all_pets >> get_birth_date
+
