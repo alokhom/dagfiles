79ab61b7ba410d1d41ff9c8a277e32d79eb1dce0
Alok Hom <alok.hom@gmail.com>
2024-11-14 10:26:16 +0100

    Update test.py

--- dags/github_archive/test.py
+++ dags/github_archive/test.py
@@ -3,9 +3,7 @@ from airflow.decorators import task
 from airflow.exceptions import AirflowException
 from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.operators.bash import BashOperator
-from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
-from airflow.utils.dates import days_ago
-from random import randint
+from airflow.operators.postgres_operator import PostgresOperator
 import datetime as dt
 import os
 import requests
@@ -64,13 +62,8 @@ with DAG(dag_id="yajl_dag_new", start_date=pendulum.datetime(2024,11,0o7,tz="CET
        getfiles_mar = PythonOperator(task_id="getfiles_mar", python_callable=my_func, op_args=['https://data.gharchive.org/2024-03-01-23.json.gz'])
 
        getfiles_jan >> getfiles_jancheck >> getfiles_feb >> getfiles_mar
-       #importall_feb >> importall_jan >> importall_mar 
 
-# db
-#execdb = PythonOperator(task_id="execdb", python_callable=execdb, op_args=['\c archive'])
-#with DAG(dag_id="postgresop_demo", schedule="@once", start_date=pendulum.datetime(2024,11,0o7,tz="CET"), catchup=False) as dag:
-
-       create_table = SQLExecuteQueryOperator(
+       create_table = PostgresOperator(
            task_id="create_table",
            conn_id="postgres_conn",
            sql="""
@@ -81,29 +74,5 @@ with DAG(dag_id="yajl_dag_new", start_date=pendulum.datetime(2024,11,0o7,tz="CET
                );
                """,
        )
-       # populate_pet_table = SQLExecuteQueryOperator(
-       #     task_id="populate_pet_table",
-       #     conn_id="postgres_conn",
-       #     sql="""
-       #          INSERT INTO pet (name, pet_type, birth_date, OWNER)
-       #          VALUES ( 'Max', 'Dog', '2018-07-05', 'Jane');
-       #          INSERT INTO pet (name, pet_type, birth_date, OWNER)
-       #          VALUES ( 'Susie', 'Cat', '2019-05-01', 'Phil');
-       #          INSERT INTO pet (name, pet_type, birth_date, OWNER)
-       #          VALUES ( 'Lester', 'Hamster', '2020-06-23', 'Lily');
-       #          INSERT INTO pet (name, pet_type, birth_date, OWNER)
-       #          VALUES ( 'Quincy', 'Parrot', '2013-08-11', 'Anne');
-       #         """,
-       # )
-       # get_all_pets = SQLExecuteQueryOperator(task_id="get_all_pets", sql="SELECT * FROM pet;")
-       # get_birth_date = SQLExecuteQueryOperator(
-       #     task_id="get_birth_date",
-       #     conn_id="postgres_conn",
-       #     sql="SELECT * FROM pet WHERE birth_date BETWEEN SYMMETRIC %(begin_date)s AND %(end_date)s",
-       #     parameters={"begin_date": "2020-01-01", "end_date": "2020-12-31"},
-       #     hook_params={"options": "-c statement_timeout=3000ms"},
-       #  )
        
        create_table 
-       #>> populate_pet_table >> get_all_pets >> get_birth_date
-
