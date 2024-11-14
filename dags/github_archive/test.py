e2dffb88801b132e72548ca1b3519b30d29f4219
Alok Hom <alok.hom@gmail.com>
2024-11-14 01:24:19 +0100

    Update test.py

--- dags/github_archive/test.py
+++ dags/github_archive/test.py
@@ -80,30 +80,31 @@ with DAG(dag_id="yajl_dag_new", start_date=pendulum.datetime(2024,11,0o7,tz="CET
                pet_type VARCHAR NOT NULL,
                birth_date DATE NOT NULL,
                OWNER VARCHAR NOT NULL);
-             """,
-       )
-       populate_pet_table = SQLExecuteQueryOperator(
-           task_id="populate_pet_table",
-           conn_id="postgres_conn",
-           sql="""
-                INSERT INTO pet (name, pet_type, birth_date, OWNER)
-                VALUES ( 'Max', 'Dog', '2018-07-05', 'Jane');
-                INSERT INTO pet (name, pet_type, birth_date, OWNER)
-                VALUES ( 'Susie', 'Cat', '2019-05-01', 'Phil');
-                INSERT INTO pet (name, pet_type, birth_date, OWNER)
-                VALUES ( 'Lester', 'Hamster', '2020-06-23', 'Lily');
-                INSERT INTO pet (name, pet_type, birth_date, OWNER)
-                VALUES ( 'Quincy', 'Parrot', '2013-08-11', 'Anne');
                """,
        )
-       get_all_pets = SQLExecuteQueryOperator(task_id="get_all_pets", sql="SELECT * FROM pet;")
-       get_birth_date = SQLExecuteQueryOperator(
-           task_id="get_birth_date",
-           conn_id="postgres_conn",
-           sql="SELECT * FROM pet WHERE birth_date BETWEEN SYMMETRIC %(begin_date)s AND %(end_date)s",
-           parameters={"begin_date": "2020-01-01", "end_date": "2020-12-31"},
-           hook_params={"options": "-c statement_timeout=3000ms"},
-        )
+       # populate_pet_table = SQLExecuteQueryOperator(
+       #     task_id="populate_pet_table",
+       #     conn_id="postgres_conn",
+       #     sql="""
+       #          INSERT INTO pet (name, pet_type, birth_date, OWNER)
+       #          VALUES ( 'Max', 'Dog', '2018-07-05', 'Jane');
+       #          INSERT INTO pet (name, pet_type, birth_date, OWNER)
+       #          VALUES ( 'Susie', 'Cat', '2019-05-01', 'Phil');
+       #          INSERT INTO pet (name, pet_type, birth_date, OWNER)
+       #          VALUES ( 'Lester', 'Hamster', '2020-06-23', 'Lily');
+       #          INSERT INTO pet (name, pet_type, birth_date, OWNER)
+       #          VALUES ( 'Quincy', 'Parrot', '2013-08-11', 'Anne');
+       #         """,
+       # )
+       # get_all_pets = SQLExecuteQueryOperator(task_id="get_all_pets", sql="SELECT * FROM pet;")
+       # get_birth_date = SQLExecuteQueryOperator(
+       #     task_id="get_birth_date",
+       #     conn_id="postgres_conn",
+       #     sql="SELECT * FROM pet WHERE birth_date BETWEEN SYMMETRIC %(begin_date)s AND %(end_date)s",
+       #     parameters={"begin_date": "2020-01-01", "end_date": "2020-12-31"},
+       #     hook_params={"options": "-c statement_timeout=3000ms"},
+       #  )
        
-       create_pet_table >> populate_pet_table >> get_all_pets >> get_birth_date
+       create_pet_table 
+       #>> populate_pet_table >> get_all_pets >> get_birth_date
 
